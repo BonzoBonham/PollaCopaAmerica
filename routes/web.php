af163ba34c77f8d651a8b110f5992dd631cef6c0
Sabeto051 <bedoya.santi@gmail.com>
2019-05-18 17:24:12 -0500

    Controladores y rutas creados

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('/equipo', 'EquipoController');
+
+Route::resource('/partido', 'PartidoController');
+
+Route::resource('/user', 'UserController');
