da8aeaeb5ef50c5dd1288b0570fabd7d45da3d76
Sabeto051 <bedoya.santi@gmail.com>
2019-05-18 10:30:24 -0500

    Modelos y Migraciones terminados

--- app/Partido.php
+++ app/Partido.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Partido extends Model
 {
-    //
     public function equipos() {
         return $this->belongsToMany('App\Equipo')->withTimestamps();
     }
