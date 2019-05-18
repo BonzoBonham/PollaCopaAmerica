dd3e20cfce023034365b8a3c9d20787fca84a7b5
Unknown <mateo.zuluaga@eia.edu.co>
2019-05-18 10:08:54 -0500

    advances on the front-end
    make a layout folder & partials  for the componets of the website 
    test some layouts

---
+++ resources/views/layouts/partials/betting-table.blade.php
@@ -0,0 +1,88 @@
+ <div class="card text-white bg-dark">
+                <div class="card-header"> NATIONAL CHAMPIONSHIP (6)</div>
+
+                <div class="card-body text-primary bg-light">
+                    <div class="container-fluid">
+                         <div class="row">
+                            <div class="col-3">
+                             <div class="container">
+                                <div class="col-1  d-inline-block">
+                                 <span style="font-size: 1em;"><i class="fas fa-futbol"></i></span>
+                                </div>
+                                <div class="col-12 d-inline-block">
+                                    <div class="row">
+                                    <div class="col-10">
+                                        <h5>real madrid</h5>
+                                    </div>
+                                    <div class="col-2">
+                                        <h5>5</h5>
+                                    </div>
+                                  </div>
+                                  <div class="row">
+                                    <div class="col-10">
+                                        <h5>chelsea</h5>
+                                    </div>
+                                    <div class="col-2">
+                                        <h5>0</h5>
+                                    </div>
+                                  </div>
+                                </div>
+                             </div>
+
+                            </div>
+                            <div class="separator"></div> 
+                            <div class="col-lg-5">
+                                  <div class="col-md-4 col-lg-5  col-xl-4  d-md-inline d-lg-inline">
+                                      <button type="button" class="btn btn-block btn-outline-primary mx-1">Real Madrid</button>
+                                  </div>
+                                  <div class="col-md-4 col-lg-5  col-xl-4 d-md-inline d-lg-inline" >
+                                      <button type="button" class="btn btn-block btn-outline-primary mx-1">Draw</button>
+                                  </div>
+                                  <div class="col-md-4 col-lg-5 col-xl-4 d-md-inline d-lg-inline" >
+                                      <button type="button" class="btn btn-block btn-outline-primary mx-1" >Chelsea</button>
+                                  </div>
+                            </div>  
+                            <div class="col-lg-2 d-lg-inline text-center">
+                             <h5 class="my-3"><span class="badge badge-pill badge-dark d-inline mx-2">+56</span><i class="fas fa-chevron-right d-inline"></i></h5>
+                            </div> 
+                        </div>
+                        <hr class="my-4">
+                          <div class="row">
+                            <div class="col-lg-3">
+                             <div class="container">
+                                <div class="row">
+                                  <div class="col-10">
+                                      <h5>real madrid</h5>
+                                  </div>
+                                  <div class="col-2">
+                                      <h5>5</h5>
+                                  </div>
+                                </div>
+                                <div class="row">
+                                  <div class="col-10">
+                                      <h5>chelsea</h5>
+                                  </div>
+                                  <div class="col-2">
+                                      <h5>0</h5>
+                                  </div>
+                                </div>
+                             </div>
+
+                            </div>
+                            <div class="separator"></div>
+                            <div class="col-lg-6">
+                              <div class="col-12">
+                                      <button type="button" class="btn btn-outline-primary mx-1">Real Madrid</button>
+                                      <button type="button" class="btn btn-outline-primary mx-1">Draw</button>
+                                      <button type="button" class="btn btn-outline-primary mx-1" >Chelsea</button>
+          
+                                </div>
+                            </div>  
+                            <div class="col-lg-2 d-lg-inline text-center">
+                             <h5 class="my-3"><span class="badge badge-pill badge-dark d-sm-inline mx-2">+56</span><i class="fas fa-chevron-right d-sm-inline"></i></h5>
+                            </div> 
+                        </div>
+                    </div>
+                </div>
+
+            </div>
---
+++ resources/views/layouts/partials/group-table.blade.php
@@ -0,0 +1,52 @@
+<div class="card text-white bg-success">
+    <div class="card-header">
+        <div class="row">
+            <div class="col-1 "> POS </div>
+            <div class="col-4 d-inline"> TEAM</div>
+            <div class="col-1"> W</div>
+            <div class="col-1"> L </div>
+            <div class="col-1"> T</div>
+            <div class="col-1"> PTS </div>
+            <div class="col-1"> DIFF </div>
+            <div class="col-1"> 89 </div>
+        </div>
+    </div>
+      <div class="card-body text-primary bg-light">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-1"><h3><small class="text-muted">01</small></h3></div>
+            <div class="col-4 d-inline">
+              <div class="container">
+                <div class="row">
+                  <div class="col-2">
+                       <span style="font-size: 2em;"><i class="fas fa-futbol"></i></span>
+                  </div>
+                  <div class="col-10">
+                      Real Madrid 
+                  </div>
+
+                </div>
+              </div>
+            </div>
+            <div class="col-1"> 45</div>
+            <div class="col-1">100</div>
+            <div class="col-1"> 6 </div>
+            <div class="col-1"> 8 </div>
+            <div class="col-1"> 9 </div>
+            <div class="col-1"> 89 </div>
+        </div>
+         <hr class="my-4">
+         <div class="row">
+            <div class="col-1"> 01 </div>
+            <div class="col-4 d-inline"> hola</div>
+            <div class="col-1"> 45</div>
+            <div class="col-1">100</div>
+            <div class="col-1"> 6 </div>
+            <div class="col-1"> 8 </div>
+            <div class="col-1"> 9 </div>
+            <div class="col-1"> 89 </div>
+        </div>
+
+    </div>
+  </div>
+</div>
