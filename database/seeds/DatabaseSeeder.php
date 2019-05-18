64a677f0c1676f8a680c4b022927791c287602e4
Unknown <mateo.zuluaga@eia.edu.co>
2019-05-18 18:33:02 -0500

    arregle un el data seeder para que solo me cree los  12 equipos

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,21 +4,29 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+
+
     /**
      * Seed the application's database.
      *
      * @return void
      */
-    public function run()
-    {
+    public function run(){
     	 $number = 200; 
         // $this->call(UsersTableSeeder::class);
 
         factory(App\User::class, 1)->states('myown')->create();
     	factory(App\User::class, $number)->create();
     	factory(App\Partido::class, $number)->create();
-    	factory(App\Equipo::class, $number)->create();
+        $this->insertEquipos();
+    }
 
-
+    public function insertEquipos(){
+         $equipos  = array( 'Brazil', 'Bolivia','Venezuela','Peru', 'Argentina', 'Colombia','Paraguay','Qatar', 'Uruguay','Ecuador','Japan','Chile');
+        foreach ($equipos as &$equipo )  { 
+            DB::table('equipos')->insert(
+                ['nombre' => $equipo ]
+            );
+        }
     }
 }
