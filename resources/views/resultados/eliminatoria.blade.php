abbe09dba56e05be5552fb4c73416a7ce6e31115
Unknown <mateo.zuluaga@eia.edu.co>
2019-05-20 13:34:27 -0500

    started the eliminatorias view

--- resources/views/resultados/eliminatoria.blade.php
+++ resources/views/resultados/eliminatoria.blade.php
@@ -2,13 +2,33 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-          <h3>FASE DE ElIMINATORIA</h3>
-          <hr class="my-4">
-          @yield('layouts.partials.group-table', compact($equipos))
-
-        </div>
-    </div>
+	<div class="row justify-content-center">
+		<div class="col-md-12">
+			<h3>FASE DE ElIMINATORIA</h3>
+			<hr class="my-4">
+			<div class="container">
+				<div class="row">
+				<div class="col-4">
+					@for ($i = 0; $i < 4; $i++)
+						@component('layouts.partials.score-card')
+						@endcomponent
+					@endfor
+				</div>
+				<div class="col-4">
+					@for ($i = 0; $i < 2 ; $i++)
+						@component('layouts.partials.score-card')
+						@endcomponent
+					@endfor
+				</div>
+				<div class="col-4">
+					@for ($i = 0; $i < 2; $i++)
+						@component('layouts.partials.score-card')
+						@endcomponent
+					@endfor
+				</div>
+				</div>
+			</div>	
+		</div>
+	</div>
 </div>
 @endsection
